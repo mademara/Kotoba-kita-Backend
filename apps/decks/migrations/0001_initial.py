d0be7e3ca06c11012426142d49a6e4f535c851cd
Mademara <mademara565@gmail.com>
2026-05-29 08:03:01 +0700

    hotfix: dependency migrasi word seharusnya Word

--- apps/decks/migrations/0001_initial.py
+++ apps/decks/migrations/0001_initial.py
@@ -7,6 +7,7 @@ class Migration(migrations.Migration):
     initial = True
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('words', '0001_initial'),
     ]
     operations = [
         migrations.CreateModel(
